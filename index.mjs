38c80de4f4245f3c631de11974ae54832b5947c4
Michael Sprague <michael@sprague.com>
2025-02-28 21:28:52 -0700

    lib route

--- index.mjs
+++ index.mjs
@@ -167,7 +167,8 @@ export default class WikiMixin extends Componentry.Module {
     }
     get library() {
         return {
-            'xipper':'/../../../xipper/xipper.bundle.js'
+            'xipper':'/xipper/xipper.bundle.js'
+            // 'xipper':'/../../../xipper/xipper.bundle.js'
         };
     }
 }
